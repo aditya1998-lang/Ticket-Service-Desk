0b9ae8f09fb8c7c2dbb0ca2d1ba192549f093d7b
Aditya gandhi <adityagandhi718@gmail.com>
2022-07-17 13:53:43 +0530

    mern project

---
+++ backend/models/ticketModel.js
@@ -0,0 +1,32 @@
+const mongoose=require('mongoose')
+
+const ticketSchema=mongoose.Schema({
+
+user:{
+    type:mongoose.Schema.Types.ObjectId,
+    required:[true,'Please add a name'],
+    ref : 'User'
+},
+product:{
+    type:String,
+    required:[true,'Please select a product'],
+    enum : [ 'iPhone','Macbook Pro' ,'iMac','iPad'],
+},
+description:{
+    type: String,
+    required:[true,'Please add a description of the issue'],
+    unique:true
+},
+status:{
+    type:String,
+    required:true,
+    default:true,
+    enum:['new','open','closed'],
+    default:'new'
+}
+
+
+
+},{timestamps:true,})
+
+module.exports=mongoose.model('Ticket',ticketSchema)
---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,16 @@
+
+
+function Spinner() {
+  return (
+     <div className="loadingSpinnerContainer">
+       <div className="loadingSpinner">
+         
+         
+         </div>
+      
+    </div>
+  )
+}
+
+export default Spinner
+
